80e28294dd1b0f0f07cd67c46ec818a0b8bc9c30
steve-ryan <stevohyoung@gmail.com>
2023-08-06 03:17:22 +0300

    setting up of routes

--- simple-blog/frontend/src/App.jsx
+++ simple-blog/frontend/src/App.jsx
@@ -4,17 +4,33 @@ import Register from "./Register";
 import AdminDashboard from "./Components/Admin/Dashboard";
 import WriterDashboard from "./Components/Writer/Dashboard";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Admin/Home";
+import Posts from "./Components/Admin/Posts";
+import Writers from "./Components/Admin/Writers";
+import Profile from "./Components/Admin/Profile";
+import HomeWriter from "./Components/Writer/HomeWriter";
+import ProfileWriter from "./Components/Writer/ProfileWriter";
+import PostsWriter from "./Components/Writer/PostsWriter";
+import AddPost from "./Components/Admin/AddPost";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
-      <Routes>
-        <Route path="/writer-dash" element={<WriterDashboard />} />
-        <Route path="/admin-dash" element={<AdminDashboard />} />
+        <Route path="/admin-dash" element={<AdminDashboard />}>
+          <Route path="" element={<Home />}></Route>
+          <Route path="posts" element={<Posts />}></Route>
+          <Route path="writers" element={<Writers />}></Route>
+          <Route path="profile" element={<Profile />}></Route>
+          <Route path="addpost" element={<AddPost />}></Route>
+        </Route>
+        <Route path="/writer-dash" element={<WriterDashboard />}>
+          <Route path="" element={<HomeWriter />}></Route>
+          <Route path="writerposts" element={<PostsWriter />}></Route>
+          <Route path="writerprofile" element={<ProfileWriter />}></Route>
+        </Route>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/register" element={<Register />}></Route>
       </Routes>
     </BrowserRouter>
   );
